f4992edbf04c650045481c87885dca9f6403b8f0
Jkouj <55113479+Jkouj@users.noreply.github.com>
2023-09-26 14:43:16 -0400

    Add files via upload

---
+++ shows.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+<p><u><b>Television Show Data</b></u>
+<br><br>9 television shows in the database<br><br><table border=1>
+<b><tr><td>showid</td><td>title</td><td>premiere_year</TD><TD>network</TD>
+	<TD>creator</TD><TD>category</TD></tr></b>
+<tr><td>sh001</td><td>30 Rock</td><td>2006</td><td>NBC</td><TD>Tina Fey</td><td>comedy</td></tr>
+<tr><td>sh002</td><td>Arrested Development</td><td>2003</td><td>Fox/Netflix</td><TD>Michael Hurwitz</td><td>comedy</td></tr>
+<tr><td>sh003</td><td>Community</td><td>2009</td><td>NBC</td><TD>Dan Harmon</td><td>comedy</td></tr>
+<tr><td>sh004</td><td>Friday Night Lights</td><td>2006</td><td>NBC/DirectTV</td><TD>Peter Berg</td><td>drama</td></tr>
+<tr><td>sh005</td><td>How I Met Your Mother</td><td>2005</td><td>CBS</td><TD>Carter Bays</td><td>comedy</td></tr>
+<tr><td>sh006</td><td>Lost</td><td>2004</td><td>ABC</td><TD>J.J. Abrams</td><td>drama</td></tr>
+<tr><td>sh007</td><td>Mad Men</td><td>2007</td><td>AMC</td><TD>Matthew Weiner</td><td>drama</td></tr>
+<tr><td>sh008</td><td>Parks and Recreation</td><td>2009</td><td>NBC</td><TD>Michael Schur</td><td>comedy</td></tr>
+<tr><td>sh009</td><td>Parenthood</td><td>2010</td><td>NBC</td><TD>Jason Katims</td><td>drama</td></tr>
+</table>
+
+
+
+</body>
+</html>
